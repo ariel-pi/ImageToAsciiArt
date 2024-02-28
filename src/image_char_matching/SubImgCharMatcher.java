26faa9f2cc84d789227862e9825e2db5ea20a31c
AmielWre <158028243+AmielWre@users.noreply.github.com>
2024-02-28 10:44:58 +0200

    Update SubImgCharMatcher.java

--- src/image_char_matching/SubImgCharMatcher.java
+++ src/image_char_matching/SubImgCharMatcher.java
@@ -1,28 +1,81 @@
 package image_char_matching;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 
 public class SubImgCharMatcher {
 
-    private final HashMap<Character, Double> charBrightnessMap = new HashMap<>();
+    private  TreeMap<Double, TreeSet<Character>> charBrightnessMap = new TreeMap<>();
 
     public SubImgCharMatcher(char[] charset) {
         for (char c : charset) {
-            charBrightnessMap.put(c, 0.0);
+            addChar(c);
         }
-
     }
 
+    private double getBrightness(boolean[][] imgMatrix) {
+        int numWhitePixels = 0;
+        int numPixels = imgMatrix.length * imgMatrix[0].length;
+        for (boolean[] row : imgMatrix) {
+            for (boolean pixel : row) {
+                if (pixel) {
+                    numWhitePixels++;
+                }
+            }
+
+        }
+        return (double) numWhitePixels / numPixels;
+    }
     public char getCharByImageBrightness(double brightness) {
-        return 't'; //todo: implement
+        //todo if map is empty
+        Double closestKey;
+        Double ceilingKey = charBrightnessMap.ceilingKey(brightness);
+        Double floorKey = charBrightnessMap.floorKey(brightness);
+        if (ceilingKey == null) {
+            closestKey = floorKey;
+        }
+        else if (floorKey == null) {
+            closestKey = ceilingKey;
+        }
+        else {
+            closestKey = Math.abs(ceilingKey - brightness) < Math.abs(floorKey - brightness) ?
+                    ceilingKey : floorKey;
+        }
+        return charBrightnessMap.get(closestKey).first();
     }
 
     public void addChar(char c) {
-        charBrightnessMap.put(c, 0.0);
+        Double brightness = getBrightness(CharConverter.convertToBoolArray(c));
+        if (charBrightnessMap.containsKey(brightness)) {
+            charBrightnessMap.get(brightness).add(c);// add the character to the set of
+            // characters with the same brightness in sorted order by ascii value
+        } else {
+            TreeSet<Character> charList = new TreeSet<>();
+            charList.add(c);
+            charBrightnessMap.put(brightness, charList);
+        }
     }
 
     public void removeChar(char c) {
         charBrightnessMap.remove(c);
+    }
+
+
+    /**
+     * Normalizes the brightness of the characters in the map to be between 0 and 1.
+     * use this method just before displaying the image.
+     */
+    private void normalizeBrightness() {
+        TreeMap <Double, TreeSet<Character>> newCharBrightnessMap = new TreeMap<>();
+        Double minBrightness = charBrightnessMap.firstKey();
+        Double maxBrightness = charBrightnessMap.lastKey();
+        for (Double brightness : charBrightnessMap.keySet()) {
+            Double newBrightness = (brightness - minBrightness) / (maxBrightness - minBrightness);
+            newCharBrightnessMap.put(newBrightness, charBrightnessMap.get(brightness));
+        }
+        charBrightnessMap = newCharBrightnessMap;
     }
 
 
